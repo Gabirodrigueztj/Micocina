9be8b61ac603690c5376974c64d2ee6384f4f8b1
Gabirodrigueztj <gabirodriguez1d@outlook.com>
2023-10-05 10:57:08 -0300

    Ajustes
    Se agrego aspectos a la apariencia de la receta random y se agrego bottstrap mediante CDN

--- script.js
+++ script.js
@@ -15,10 +15,19 @@ fetch(url)
     if (receta) {
       // Crea el contenido HTML para mostrar la receta aleatoria
       const html = `
+      <div class="container">
+      <div class="row">
         <h2>${receta.strMeal}</h2>
-        <img src="${receta.strMealThumb}" alt="${receta.strMeal}" />
-        <p class="instrucciones">${receta.strInstructions}</p>
-      `;
+        
+      <div class="col-md-8">
+        <img src="${receta.strMealThumb}" alt="${receta.strMeal}" class="img-fluid">
+      </div>
+      <div class="col-md-4">
+      <h3>Pasos a seguir:</h3>
+      <p class="instrucciones">${receta.strInstructions}</p>
+      </div>
+      </div>
+      </div>`;
 
       // Agrega el contenido HTML al contenedor
       recetaAleatoriaContainer.innerHTML = html;
@@ -30,6 +39,53 @@ fetch(url)
   .catch(error => {
     console.error('Error al obtener la receta aleatoria:', error);
   });
+
+// *********** comienza resultado html*******
+const buscarBtn = document.getElementById('buscar-btn');
+const campoBusqueda = document.getElementById('busqueda');
+const resultadosContainer = document.getElementById('resultados');
+
+buscarBtn.addEventListener('click', function () {
+  const valorBusqueda = campoBusqueda.value.trim();
+  
+  if (valorBusqueda !== '') {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${encodeURIComponent(valorBusqueda)}`;
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        mostrarResultados(data);
+      })
+      .catch(error => {
+        console.error('Error al realizar la búsqueda:', error);
+      });
+  } else {
+    resultadosContainer.innerHTML = 'Por favor, ingresa el nombre de la comida.';
+  }
+});
+
+function mostrarResultados(data) {
+  resultadosContainer.innerHTML = '';
+
+  if (data.meals && data.meals.length > 0) {
+    data.meals.forEach(meal => {
+      const comidaHTML = `
+        <h2>${meal.strMeal}</h2>
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}"  />
+        <p>${meal.strCategory}</p>
+        <a href="${meal.strSource}" target="_blank">Ver receta</a>
+      `;
+
+      const resultadoElement = document.createElement('div');
+      resultadoElement.classList.add('resultado');
+      resultadoElement.innerHTML = comidaHTML;
+      resultadosContainer.appendChild(resultadoElement);
+    });
+  } else {
+    resultadosContainer.innerHTML = 'No se encontraron resultados para la búsqueda.';
+  }
+}
+
 
  
 
